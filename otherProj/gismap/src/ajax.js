7e8407cc38382fbe9e7e78af646da46f56e7baf3
Yelly <dingye621@126.com>
2020-04-09 18:08:59 +0800

    ajax方法更新

--- otherProj/gismap/src/ajax.js
+++ otherProj/gismap/src/ajax.js
@@ -1,11 +1,25 @@
-function getTagInfo(tagName)
+// function getTagInfo(tagName)
+// {
+//     $.ajax({  
+//         type : "get",  //提交方式  
+//         url : "http://" + globalConfig.ip + "/api/Tag/getDto?name="+tagName,//路径  
+//         async: false,
+//         data : {},//数据，这里使用的是Json格式进行传输  
+//         success : function(result) {//返回数据根据结果进行相应的处理  
+//             return result;
+//         }  
+//     });  
+// }
+
+async function getTagInfo(tagName)
 {
-    $.ajax({  
-        type : "get",  //提交方式  
-        url : "http://" + globalConfig.ip + "/api/Tag/getDto?name="+tagName,//路径  
-        data : {},//数据，这里使用的是Json格式进行传输  
-        success : function(result) {//返回数据根据结果进行相应的处理  
-            return result;
-        }  
-    });  
+    return await axios.get("http://" + globalConfig.ip + "/api/Tag/getDto?name="+tagName);
+//   .then(function (response) {
+//       console.log('res..',response);
+//    return response;
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 }
+
